3b754d668368c642a4734feed827bdef92ba70e3
dhanush-2412 <dhanushshetty2412@gmail.com>
2024-12-06 23:34:08 +0530

    Add zsh configuration

--- stow.sh
+++ stow.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+# ========== Dependencies ==========
+packages=()
+
+# zsh
+packages+=(fzf kitty stow tmux zoxide zsh)
+
+# Install dependencies
+yay -S --needed "${packages[@]}"
+
+# ========== Configurations ==========
+
+# zsh
+stow zsh
+[ "$SHELL" != "$(which zsh)" ] && chsh -s "$(which zsh)"
+source "$HOME/.zshrc"
