c2579d5b26324f8d112e87d64310644eaa2df0db
dhanush-2412 <dhanushshetty2412@gmail.com>
2024-12-08 19:33:59 +0530

    Add vscode configuration

--- stow.sh
+++ stow.sh
@@ -7,6 +7,8 @@ packages=()
 packages+=(fzf kitty stow tmux zoxide zsh)
 # tmux
 packages+=(tmux)
+# vscode
+packages+=(visual-studio-code-bin)
 
 # Install dependencies
 yay -S --needed "${packages[@]}"
@@ -22,3 +24,19 @@ source "$HOME/.zshrc"
 load https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 stow tmux
 ~/.tmux/plugins/tpm/bin/install_plugins
+
+# vscode
+extensions=(
+    "catppuccin.catppuccin-vsc"
+    "eamodio.gitlens"
+    "esbenp.prettier-vscode"
+    "github.copilot-chat"
+    "github.copilot"
+    "ms-python.vscode-pylance"
+    "pkief.material-icon-theme"
+)
+for extension in "${extensions[@]}"; do
+    code --install-extension "$extension"
+done
+
+stow vscode
